457d2c4e60cf7a5429be6474968f957165de2c6b
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-05-24 12:13:53 +0300

    Adding tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Rida Arif', email: 'example@mail.com', password: 'password') }
+  let(:group) { Group.create(name: 'Shopping', icon: 'https://i.imgur.com/Ar3Lf3Dt.png', user_id: user.id) }
+
+  describe 'Validations' do
+    context 'when valid' do
+      it { expect(group).to be_valid }
+    end
+
+    it 'should allow valid name' do
+      group.name = 'Eg'
+      expect(group).to_not be_valid
+    end
+
+    it 'should allow valid name' do
+      group.name = 'Outing'
+      expect(group).to be_valid
+    end
+
+    it 'should allow valid user_id' do
+      group.user_id = nil
+      expect(group).to_not be_valid
+    end
+  end
 end
